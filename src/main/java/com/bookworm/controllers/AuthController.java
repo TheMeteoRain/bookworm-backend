76c179e22f323b57aa499b8970f88a0f3f672094
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-04-24 17:25:47 +0300

    Add cors configuration for authentication.

--- src/main/java/com/bookworm/controllers/AuthController.java
+++ src/main/java/com/bookworm/controllers/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@CrossOrigin(exposedHeaders = "Authorization")
 @RestController
 public class AuthController {
 
@@ -36,6 +37,7 @@ public class AuthController {
                 return "Successfully logged in";
             }
         }
+        res.setStatus(403);
         return "Login failed";
     }
 
