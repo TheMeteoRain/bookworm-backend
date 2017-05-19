a3f075be8b412f9691637e0424a8c75163bcc75c
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-19 12:15:09 +0300

    Add GlobalCorsConfiguration.

---
+++ src/main/java/com/bookworm/GlobalCorsConfiguration.java
@@ -0,0 +1,27 @@
+package com.bookworm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Configures the applications CORS mappings.
+ *
+ * @author Toni Seppäläinen toni.seppalainen@cs.tamk.fi
+ * @version 2017.0519
+ * @since 1.7
+ */
+@Configuration
+public class GlobalCorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
