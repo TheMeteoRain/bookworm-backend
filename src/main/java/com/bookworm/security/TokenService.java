163fcf068e652761c85866a37522c36d041e5e72
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-24 12:36:15 +0300

    Add javadoc to TokenService.

--- src/main/java/com/bookworm/security/TokenService.java
+++ src/main/java/com/bookworm/security/TokenService.java
@@ -1,24 +1,49 @@
 package com.bookworm.security;
 
 import com.bookworm.models.Member;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
+/**
+ * Verifies and creates JWT tokens.
+ *
+ * @author Toni Seppäläinen toni.seppalainen@cs.tamk.fi
+ * @version 2017.0522
+ * @since 1.7
+ */
 public class TokenService {
 
+    /**
+     * Secret used as a JWT key.
+     */
     private static final String JWT_KEY = "super_secret";
 
+    /**
+     * Creates a JWT token from {@link Member}
+     *
+     * @param member Member to create token for.
+     * @return Created JWT token.
+     */
     public static String createToken(Member member) {
 
         Claims claims = Jwts.claims().setSubject(member.getUsername());
         claims.put("id", member.getMemberId());
         claims.put("username", member.getUsername());
         claims.put("admin", member.isAdmin());
-        return "Bearer " +  Jwts.builder()
+        return "Bearer " + Jwts.builder()
                 .setClaims(claims)
                 .signWith(SignatureAlgorithm.HS512, JWT_KEY)
                 .compact();
     }
 
+    /**
+     * Checks if a token is valid.
+     *
+     * @param token Token to check.
+     * @return True if token is valid, false otherwise.
+     */
     public static AuthenticatedUser verify(String token) {
         if (token != null) {
             try {
@@ -26,8 +51,8 @@ public class TokenService {
                 token = token.substring(7);
                 Jws<Claims> claims = Jwts.parser().setSigningKey(JWT_KEY).parseClaimsJws(token);
                 int id = (int) claims.getBody().get("id");
-                String username = (String)claims.getBody().get("username");
-                boolean admin = (boolean)claims.getBody().get("admin");
+                String username = (String) claims.getBody().get("username");
+                boolean admin = (boolean) claims.getBody().get("admin");
                 return new AuthenticatedUser(id, username, admin);
             } catch (Exception e) {
                 e.printStackTrace();
