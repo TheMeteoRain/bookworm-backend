95053b726f2e3b497b512c7ec6c8260429003485
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-24 12:10:03 +0300

    Add javadoc to AuthorController.

--- src/main/java/com/bookworm/controllers/AuthorController.java
+++ src/main/java/com/bookworm/controllers/AuthorController.java
@@ -8,17 +8,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Provides search access to authors in database.
+ *
+ * @author Toni Seppäläinen toni.seppalainen@cs.tamk.fi
+ * @version 2017.0522
+ * @since 1.7
+ */
 @RestController
 @RequestMapping(value = "/authors")
 public class AuthorController {
 
+    /**
+     * Handle to author repository.
+     */
     @Autowired
     AuthorRepository database;
 
+    /**
+     * Empty constructor for Spring.
+     */
     public AuthorController() {
 
     }
 
+    /**
+     * Retrieves all authors matching the search term.
+     *
+     * @param term The input to search with.
+     * @return All found authors.
+     */
     @RequestMapping(method = RequestMethod.GET)
     public Iterable<Author> getAuthors(@RequestParam("search") String term) {
         if (term != null) {
