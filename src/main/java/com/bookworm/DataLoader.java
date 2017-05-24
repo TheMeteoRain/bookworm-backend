d8d12b6dd9238a950e7ed74c1cddf3a2003596bf
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-24 12:39:39 +0300

    Add javadoc to DataLoader.

--- src/main/java/com/bookworm/DataLoader.java
+++ src/main/java/com/bookworm/DataLoader.java
@@ -8,7 +8,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 /**
- * 
+ * Loads data at the start up time of the application.
  * 
  * @version 2017.0522
  * @author Toni Seppäläinen toni.seppalainen@cs.tamk.fi
@@ -17,13 +17,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements ApplicationRunner{
 
+    /**
+     * Authentication controller.
+     */
     private AuthController authController;
 
+    /**
+     * Constructs the instance.
+     *
+     * @param authController Handle to {@link AuthController}.
+     */
     @Autowired
     public DataLoader(AuthController authController) {
         this.authController = authController;
     }
 
+    /**
+     * Gets called when the application starts to load data.
+     *
+     * @param applicationArguments Arguments for the application.
+     * @throws Exception Exception thrown by Spring.
+     */
     @Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
         authController.registerMember(new MemberRegisterDetails("bookworm.mail.service@gmail.com", "admin", "admin"));
